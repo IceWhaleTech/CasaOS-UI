3c1fe80d9e5b92febdc5b89fd1f39d74d8d2f557
老竭力 <jerrykuku@qq.com>
2022-01-28 16:12:44 +0800

    🐞 fix: Fix language
    Fixed the language initialization bug

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@
  * @Author: JerryK
  * @Date: 2021-09-18 21:32:13
  * @LastEditors: JerryK
- * @LastEditTime: 2022-01-28 11:13:11
+ * @LastEditTime: 2022-01-28 16:10:50
  * @Description: 
  * @FilePath: /CasaOS-UI/src/store/index.js
  */
@@ -64,7 +64,6 @@ export default new Vuex.Store({
       state.siteLoading = false
     },
     changeInitialization(state, val) {
-      console.log("init", val);
       state.needInitialization = val
     },
     changeHardwareInfo(state, val) {
