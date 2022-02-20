da76850809e90f713e76431a50bc4f4a0fcea046
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2022-02-20 14:22:25 -0500

    Removed unnecessary space in BrandBar.vue

--- src/components/BrandBar.vue
+++ src/components/BrandBar.vue
@@ -13,7 +13,7 @@
       <img :src="require('@/assets/img/casa.svg')">
     </figure>
     <span class="is-size-4 mr-3 ml-3">CasaOS</span>
-    <span>Made by IceWhale with ❤ and you !️</span>
+    <span>Made by IceWhale with ❤ and you!️</span>
   </div>
 </template>
 
