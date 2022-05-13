69d4c1ba58c23999d32482476195890dbb8d1bf1
老竭力 <jerrykuku@qq.com>
2022-05-13 20:20:41 +0800

    🐞 Fixed - [Files] Fix the problem that PDF files cannot be previewed.

--- public/static.go
+++ public/static.go
@@ -1,6 +1,16 @@
+/*
+ * @Author: 老竭力 jerrykuku@qq.com
+ * @Date: 2022-02-18 10:20:10
+ * @LastEditors: 老竭力 jerrykuku@qq.com
+ * @LastEditTime: 2022-05-13 20:12:39
+ * @FilePath: \CasaOS-UI\public\static.go
+ * @Description:
+ *
+ * Copyright (c) 2022 by 老竭力 jerrykuku@qq.com, All Rights Reserved.
+ */
 package web
 
 import "embed"
 
-//go:embed index.html favicon.svg browserconfig.xml site.webmanifest robots.txt img js fonts
+//go:embed index.html favicon.svg browserconfig.xml site.webmanifest robots.txt img js fonts *.worker.js
 var Static embed.FS
