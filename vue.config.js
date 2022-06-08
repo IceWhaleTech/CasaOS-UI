08ef495da7355fa8ec97d0cd3fe08bd783000a10
老竭力 <jerrykuku@qq.com>
2022-06-08 19:29:58 +0800

    🐳 chore: Disable analyzer

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 /*
  * @Author: Jerryk jerry@icewhale.org
  * @Date: 2022-02-18 10:20:10
- * @LastEditors: Jerryk jerry@icewhale.org
- * @LastEditTime: 2022-06-02 19:18:46
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-06-08 19:28:05
  * @FilePath: \CasaOS-UI\vue.config.js
  * @Description: 
  * 
@@ -39,8 +39,8 @@ module.exports = {
         // 
         if (process.env.NODE_ENV === "prod") {
             config.optimization.minimize(true);
-            config.plugin('webpack-bundle-analyzer')
-                .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin)
+            // config.plugin('webpack-bundle-analyzer')
+            //     .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin)
             config.optimization.splitChunks({
                 chunks: 'all'
             })
