e3140b65d4bfe19f0524f2637b998eed8f1c2009
zhanghengxin <ezreal.ice@icloud.com>
2022-11-04 21:26:33 +0800

    ⭐️ feat · 0.3.8 · rss -- support for rss

--- src/components/BrandBar.vue
+++ src/components/BrandBar.vue
@@ -15,12 +15,16 @@
     <span class="logo-text mr-3 ml-1">CasaOS</span>
     <span v-if="!rssShow && rss.length" class="intro-text">Made with ❤️ by IceWhale and YOU!</span>
     <span v-else class="window">
-      <ul class="scroll" :style="{'--time': 5*rss.length+'s'}">
+      <ul class="scroll" :style="{'--time': 5*line+'s', '--perc': perc, '--line': line}">
         <li v-for="(item,key) in rss" :key="key"><a :href="item.link" target="_blank" class="intro-text">{{
             item.title
           }}</a></li>
+        <li :key="rss.length" v-if="rss.length > 1"><a :href="rss[0].link" target="_blank" class="intro-text">{{
+            rss[0].title
+          }}</a></li>
       </ul>
     </span>
+
   </div>
 </template>
 
@@ -29,9 +33,16 @@ import Parser from "rss-parser";
 
 export default {
   name: "brand-bar",
+  components: {},
   computed: {
     rssShow() {
       return this.$store.state.rssSwitch
+    },
+    line() {
+      return this.rss.length === 1 ? 1 : this.rss.length + 1
+    },
+    perc() {
+      return ((1 - this.line) / this.line).toFixed(2) * 100 + '%'
     },
   },
   data() {
@@ -40,13 +51,17 @@ export default {
     };
   },
   created() {
-    console.log(123)
     this.parseFeed()
   },
   methods: {
     async parseFeed() {
       let parser = new Parser();
-      let feed = await parser.parseURL('https://blog.casaos.io/feed/')
+      let params = await this.$api.file.getContent('/var/lib/casaos/baseinfo.conf').then(res => {
+        return JSON.parse(res.data.data)
+      })
+      params.l = localStorage.getItem('lang') ? localStorage.getItem('lang') : navigator.language.toLowerCase().replace("-", "_");
+      let stringify = btoa(encodeURIComponent(JSON.stringify(params)))
+      let feed = await parser.parseURL('https://blog.casaos.io/feed/?key=' + stringify);
       this.rss = feed.items
     }
   }
@@ -96,28 +111,53 @@ export default {
     transform: translate(0, 0);
   }
   100% {
-    transform: translate(0, -100%)
+    transform: translate(0, var(--perc));
+    //transform: translate(0, -100%);
   }
 }
 
 .window {
   height: 1rem;
   overflow: hidden;
-
 }
 
 .scroll {
   width: 100%;
+  //height: var(--line) rem;
   line-height: 1rem;
-  verticle-align: bottom;
+  //verticle-align: bottom;
   text-align: center;
   animation-name: scroll;
   animation-duration: var(--time);
   animation-delay: 0s;
   animation-iteration-count: infinite;
+  animation-timing-function: steps(var(--line), jump-none);
 
   li {
+    max-width: 15rem;
+    height: 1rem;
+    overflow: hidden;
+    text-overflow: ellipsis;
     no-wrap: true;
+    //white-space: nowrap;
+    //animation-name: scroll;
+    //animation-duration: var(--time);
+    //animation-delay: 0s;
+    //animation-iteration-count: infinite;
+    //animation-timing-function: cubic-bezier(0.1, -0.6, 0.2, 0);
   }
+
+  li:hover {
+    text-decoration: underline;
+
+  }
+}
+
+.scroll:hover {
+  animation-play-state: paused;
+}
+
+.aaa {
+  height: 500px;
 }
 </style>
