1625a61ec1fb4e80c1078ac2a728f3c4ebc937b0
zhanghengxin <ezreal.ice@icloud.com>
2022-11-17 14:47:14 +0800

    ⭐️ feat · 0.3.8 · Add multiple languages

--- src/assets/lang/index.js
+++ src/assets/lang/index.js
@@ -9,7 +9,27 @@ import pl_pl from './pl_PL.json'
 import pt_br from './pt_BR.json'
 import hu_hu from './hu_HU.json' // Hungarian: Magyar
 import sv_se from './sv_SE.json' // Swedish: Svenska
+import be_by from './be_BY.json' // Belarusian: Беларуская
+import id_id from './id_ID.json' // Indonesian: Bahasa Indonesia
+import ja_jp from './ja_JP.json' // Japanese: やまと
+import ko_kr from './ko_KR.json' // Korean: 한국어
+import nb_no from './nb_NO.json' // Norwegian: Norsk
 
 export default {
-    zh_cn, en_us, ru_ru, fr_fr, es_es, de_de, it_it, pt_br, pl_pl, hu_hu, sv_se
+    zh_cn,
+    en_us,
+    ru_ru,
+    fr_fr,
+    es_es,
+    de_de,
+    it_it,
+    pt_br,
+    pl_pl,
+    hu_hu,
+    sv_se,
+    be_by,
+    id_id,
+    ja_jp,
+    ko_kr,
+    nb_no
 }
