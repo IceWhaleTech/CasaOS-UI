edadd19f20836a8d76bbf1565fda52985c7c9e8d
zhanghengxin <ezreal.ice@icloud.com>
2022-11-22 20:49:06 +0800

    0.4.0 2022-11-22

--- src/components/noticBlock/noticeBlock.vue
+++ src/components/noticBlock/noticeBlock.vue
@@ -13,55 +13,54 @@
 <template>
   <div class="common-card">
     <div class="blur-background"></div>
-    <div class="content widget">
-      <!-- Init State Start -->
-      <div class="widget-header is-flex">
+    <div class="content widget _card is-flex is-flex-direction-column">
+      <!-- start of section head-->
+      <div class="widget-header is-flex is-flex-shrink-0">
         <div class="image is-24x24">
           <img :src="require('@/assets/img/logo/casa-white.svg')"/>
         </div>
-        <div class="widget-title pl-2">
+        <div class="header-title pl-2">
           {{ $t("Find New Drive") }}
         </div>
       </div>
+      <!-- end of section head-->
 
-      <div class="is-flex is-align-items-center">
-        <div class="info">
-          <div class="widget-header is-flex _content-margin">
-            <div class="image is-24x24">
-              <img :src="require('@/assets/img/logo/casa-white.svg')"/>
-            </div>
-            <div class=" is-flex is-flex-grow-1 ml-2">
-              <div class="widget-title is-flex-grow-1 nowarp">
-                {{ $t("Find New Drive") }}
-              </div>
-              <div class="has-text-left is-size-14px mt-1 is-flex-shrink-0">
-                <span class="op65">100G/1000G</span>
-              </div>
-            </div>
+      <!-- start of section content-->
+      <div
+          class="info is-flex is-flex-direction-column is-justify-content-space-between is-flex-grow-1">
+        <div class="widget-content is-flex mr-0">
+          <div class="image is-24x24 is-flex-shrink-0">
+            <img :src="require('@/assets/img/logo/casa-white.svg')"/>
           </div>
-          <div class="widget-header is-flex scrpe-margin">
-            <div class="image is-24x24">
-              <img :src="require('@/assets/img/logo/casa-white.svg')"/>
-            </div>
-            <div class=" is-flex is-flex-grow-1 ml-2">
-              <div class="widget-title is-flex-grow-1 nowarp">
-                {{ $t("Find New Drive") }}
-              </div>
-              <p class="has-text-left is-size-14px mt-1 is-flex-shrink-0">
-                <span class="op65">100G/1000G</span>
-              </p>
-            </div>
+          <div class="content-title is-flex-grow-1 nowarp ml-2">
+            {{ $t("Find New Drive") }}
           </div>
-
+          <div class="has-text-left is-size-14px mt-1 is-flex-shrink-0">
+            <span class="op65">100G/1000G</span>
+          </div>
+        </div>
+        <div class="line _ml-2rem"></div>
+        <div class="widget-content is-flex mr-0">
+          <div class="image is-24x24 is-flex-shrink-0">
+            <img :src="require('@/assets/img/logo/casa-white.svg')"/>
+          </div>
+          <div class="content-title is-flex-grow-1 nowarp ml-2">
+            {{ $t("Find New Drive") }}
+          </div>
+          <p class="has-text-left is-size-14px mt-1 is-flex-shrink-0">
+            <span class="op65">100G/1000G</span>
+          </p>
         </div>
       </div>
-      <div class="is-flex is-flex-direction-row-reverse">
+      <!-- end of section content-->
+
+      <!-- start of section footer-->
+      <div class="is-flex is-flex-direction-row-reverse is-flex-shrink-0">
         <b-button :disabled="false" class="width" rounded size="is-small" type="is-primary" @click="TODO">
           {{ $t('Set MainStorage') }}
         </b-button>
       </div>
-      <!-- Init State End -->
-
+      <!-- end of section footer-->
     </div>
   </div>
 </template>
@@ -85,8 +84,7 @@ export default {
   beforeDestroy() {
     this.$EventBus.$off(events.UPDATE_SYNC_STATUS);
   },
-  computed: {
-  },
+  computed: {},
 
   methods: {
 
@@ -177,36 +175,48 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-// copy from src/assets/scss/common/_others.scss:102
+._card {
+  height: 11.5rem;
+
+  .widget-header {
+    .header-title {
+      font-family: 'Roboto';
+      font-style: normal;
+      font-weight: 500;
+      font-size: 1.125rem;
+      line-height: 1.75rem;
+      color: hsla(208, 16%, 96%, 1);
+    }
+  }
+
+  .widget-content {
+    .content-title {
+      font-family: 'Roboto';
+      font-style: normal;
+      font-weight: 500;
+      font-size: 14px;
+      line-height: 20px;
+      color: hsla(208, 16%, 96%, 1);
+    }
+  }
+}
+
 .content {
-  position: relative;
   z-index: 10;
   padding: 1rem;
   padding-left: 1.25rem;
   padding-right: 1.25rem;
-  padding-bottom: 0.75rem;
 
   .info {
-    //margin: 1.5rem;
-    margin-top: 0.75rem;
-    margin-bottom: 0.75rem;
-    margin-left: 2rem;
-    margin-right: 0rem;
+    margin: 1.5rem 2rem;
 
-    ._content-margin {
-      margin-top: 0.75rem;
-      margin-bottom: 0.75rem;
-      padding-bottom: 0.75rem;
+    ._ml-2rem {
+      margin-left: 2rem;
     }
 
-    //src/assets/scss/common/_others.scss:24
-    .b-line {
-      border-bottom: #cfcfcf 1px solid;
-      margin-right: 2rem;
-
-      .nowarp {
-        white-space: nowrap;
-      }
+    .line {
+      width: auto;
+      border-top: 1px solid rgba(255, 255, 255, 0.1);
     }
   }
 }
