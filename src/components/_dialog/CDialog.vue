23041613d9c79ebe3988359b62ec207bd5d157c6
zhanghengxin <ezreal.ice@icloud.com>
2022-11-27 11:14:08 +0800

    0.4.0 2022-11-26 fixed IceWhaleTech/CasaOS#703

---
+++ src/components/_dialog/CDialog.vue
@@ -0,0 +1,27 @@
+<!--
+  - Copyright (c) 2022. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+  - Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+  - Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+  - Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+  - Vestibulum commodo. Ut rhoncus gravida arcu.
+  -->
+
+<template>
+  <b-modal v-model="show" width="20rem" scroll="clip">
+    <b-message>
+      <template #header>
+        <img alt="pending" :src="require('@/assets/img/widgets/swiper-right.svg')"/>
+      </template>
+    </b-message>
+  </b-modal>
+</template>
+
+<script>
+export default {
+  name: "CDialog.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
