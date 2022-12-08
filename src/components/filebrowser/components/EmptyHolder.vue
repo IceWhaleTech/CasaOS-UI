82f7e4f0db28d22897eadcbdd859292e3f3f3cf5
zhanghengxin <ezreal.ice@icloud.com>
2022-12-08 15:43:36 +0800

    :bug: · 0.4.0 · fix IceWhaleTech/CasaOS#738

--- src/components/filebrowser/components/EmptyHolder.vue
+++ src/components/filebrowser/components/EmptyHolder.vue
@@ -1,49 +1,55 @@
 <!--
- * @Author: JerryK
- * @Date: 2022-03-07 17:10:25
- * @LastEditors: Jerryk jerry@icewhale.org
- * @LastEditTime: 2022-05-27 08:59:57
- * @Description: 
- * @FilePath: \CasaOS-UI\src\components\filebrowser\components\EmptyHolder.vue
--->
+  * @LastEditors: zhanghengxin ezreal.zhang@icewhale.org
+  * @LastEditTime: 2022/12/8 下午3:10
+  * @FilePath: /CasaOS-UI/src/components/filebrowser/components/EmptyHolder.vue
+  * @Description:
+  *
+  * Copyright (c) 2022 by IceWhale, All Rights Reserved.
+  -->
 <template>
-  <div class="has-text-centered">
+	<div class="has-text-centered">
 
-    <h5 class="title is-65">{{ $t('Drop your files here to upload') }}</h5>
-    <p class="is-size-7">{{ $t('or') }}</p>
+		<h5 class="title is-65">{{ $t('Drop your files here to upload') }}</h5>
+		<p class="is-size-7">{{ $t('or') }}</p>
 
-    <ul class="is-flex upload-btns is-justify-content-center">
-      <li class="e-btn" @click="$emit('newFile')">
-        <span>{{ $t('New File') }}</span>
-        <b-image :src="require('@/assets/img/filebrowser/newFile.svg')"></b-image>
-      </li>
-      <li class="e-btn" @click="$emit('newFolder')">
-        <span>{{ $t('New Folder') }}</span>
-        <b-image :src="require('@/assets/img/filebrowser/newFolder.svg')"></b-image>
-      </li>
-      <li class="e-btn" id="eUpFileBtn">
-        <span>{{ $t('Upload Files') }}</span>
-        <b-image :src="require('@/assets/img/filebrowser/upfile.svg')"></b-image>
-      </li>
-      <li class="e-btn" id="eUpFolderBtn">
-        <span>{{ $t('Upload Folder') }}</span>
-        <b-image :src="require('@/assets/img/filebrowser/upfolder.svg')"></b-image>
-      </li>
-    </ul>
-  </div>
+		<ul class="is-flex upload-btns is-justify-content-center">
+			<li class="e-btn" @click="$emit('newFile')">
+				<span>{{ $t('New File') }}</span>
+				<b-image :src="require('@/assets/img/filebrowser/newFile.svg')"></b-image>
+			</li>
+			<li class="e-btn" @click="$emit('newFolder')">
+				<span>{{ $t('New Folder') }}</span>
+				<b-image :src="require('@/assets/img/filebrowser/newFolder.svg')"></b-image>
+			</li>
+			<li id="eUpFileBtn" class="e-btn">
+				<span>{{ $t('Upload Files') }}</span>
+				<b-image :src="require('@/assets/img/filebrowser/upfile.svg')"></b-image>
+			</li>
+			<li id="eUpFolderBtn" class="e-btn" @click="freshPath">
+				<span>{{ $t('Upload Folder') }}</span>
+				<b-image :src="require('@/assets/img/filebrowser/upfolder.svg')"></b-image>
+			</li>
+		</ul>
+	</div>
 </template>
 
 <script>
-import { mixin } from '@/mixins/mixin';
+import {mixin} from '@/mixins/mixin';
+
 export default {
-  mixins: [mixin],
-  inject: ['filePanel'],
-  mounted() {
-    this.$nextTick(() => {
-      this.filePanel.uploaderInstance.assignBrowse(document.getElementById('eUpFileBtn'))
-      this.filePanel.uploaderInstance.assignBrowse(document.getElementById('eUpFolderBtn'), true)
-    });
-  },
+	mixins: [mixin],
+	inject: ['filePanel'],
+	mounted() {
+		this.$nextTick(() => {
+			this.filePanel.uploaderInstance.assignBrowse(document.getElementById('eUpFileBtn'))
+			this.filePanel.uploaderInstance.assignBrowse(document.getElementById('eUpFolderBtn'), true)
+		});
+	},
+	methods: {
+		freshPath() {
+			this.filePanel.uploaderInstance.opts.query.path = this.filePanel.currentPath
+		}
+	}
 }
 </script>
 
