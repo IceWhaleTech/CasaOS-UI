0c95b31c0634ed3af6dedba4dc95911490c95b45
zhanghengxin <ezreal.ice@icloud.com>
2022-12-12 14:55:08 +0800

    🚧  · 0.4.0 · Updating the BrandBar.vue and style files.
    Fix IceWhaleTech/CasaOS#748

--- src/components/BrandBar.vue
+++ src/components/BrandBar.vue
@@ -11,7 +11,6 @@
 		<figure class="image _is-125x24">
 			<img :src="require('@/assets/img/logo/logo.svg')">
 		</figure>
-		<!--    <span class="logo-text mr-3 ml-1">CasaOS</span>-->
 		<span v-if="!rssShow && rss.length" class="intro-text ml-4">Made with ❤️ by IceWhale and YOU!</span>
 		<span v-else class="window ml-4 is-flex is-align-items-center">
       <ul :style="{'--time': 5*line+'s', '--perc': perc, '--line': line}" class="scroll">
