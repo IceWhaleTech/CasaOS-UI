c8f66d08f8c5de4be2fa191c3b954f0159b13d8b
zhanghengxin <ezreal.zhang@icewhale.org>
2022-12-22 00:48:28 +0800

    :construction: Work in progress

--- src/router/route.js
+++ src/router/route.js
@@ -52,7 +52,7 @@ export default [
         path: '/developmentElement',
         name: 'DevelopmentElement',
         hidden: true,
-        component: () => import('@/components/Storage/MergeStorages.vue'),
+        component: () => import('@/views/Login.vue'),
         meta: {
             requireAuth: true,
             showBackground: false
