10f10c83630a4c243a9629b81faa9f697a5fcb39
zhanghengxin <812718649@qq.com>
2023-02-24 11:18:37 +0800

    V0.4.2 hotfix2 (#119)

--- src/mixins/app/Business_OpenThirdApp.js
+++ src/mixins/app/Business_OpenThirdApp.js
@@ -12,17 +12,9 @@ import container from "@/service/container";
 export default {
     methods: {
         openAppToNewWindow(appInfo) {
-            this.removeIdFromSessionStorage(appInfo.id);
-            let routeUrl = this.$router.resolve({
-                name: 'AppLauncherCheck',
-                path: '/launch',
-                query: {
-                    appDetailData: JSON.stringify(appInfo)
-                }
-            });
-            window.open(routeUrl.href, '_blank');
+            this.hasNewTag(appInfo.id) ? this.firstOpenThirdApp(appInfo) : this.openThirdApp(appInfo, true);
         },
-        openThirdApp(appInfo) {
+        openThirdApp(appInfo, isNewWindows) {
             this.$messageBus('apps_open', appInfo.name);
             if (appInfo.host !== "" || appInfo.port !== "" || appInfo.index !== "") {
                 const hostIp = appInfo.host || this.$baseIp
@@ -49,7 +41,10 @@ export default {
                 //     return
                 // }
                 if (appInfo.image.toLowerCase().indexOf("qbittorrent") === -1) {
-                    window.location.replace(url);
+                    isNewWindows ? window.open(url, '_blank') : window.location.replace(url);
+                } else if (isNewWindows) {
+                    var arg = '\u003cscript\u003elocation.replace("' + url + '")\u003c/script\u003e';
+                    window.open('javascript:window.name;', arg);
                 } else {
                     let html = document.createElement('a');
                     html.href = url;
@@ -67,5 +62,16 @@ export default {
             let containerInfo = await container.getMyAppList(data).then(res => res.data.data.casaos_apps)
             this.openAppToNewWindow(containerInfo[0])
         },
+        firstOpenThirdApp(appInfo) {
+            this.removeIdFromSessionStorage(appInfo.id);
+            let routeUrl = this.$router.resolve({
+                name: 'AppLauncherCheck',
+                path: '/launch',
+                query: {
+                    appDetailData: JSON.stringify(appInfo)
+                }
+            });
+            window.open(routeUrl.href, '_blank');
+        }
     }
 }
