2891cd073fb0a52705af9874794a72400da80ab4
zhanghengxin <ezreal.zhang@icewhale.org>
2023-03-15 10:59:06 +0800

    💄: adjust the height

--- src/components/noticBlock/noticeBlock.vue
+++ src/components/noticBlock/noticeBlock.vue
@@ -23,22 +23,23 @@
 				<div class="header-title pl-2 is-flex-grow-1">
 					{{ $t(noticeData.prelude.title) }}
 				</div>
-				<div class="is-flex-shrink-0 _close-polymorphic" @click="close">
+				<div class="is-flex-shrink-0 _close-polymorphic" style="height: 1.5rem" @click="close">
 					<b-icon custom-size="casa-24px" icon="close-xs" pack="casa"></b-icon>
 				</div>
 			</div>
 			<!-- end of section head-->
-
+			
 			<!-- start of section body-->
 			<!--			list-->
 			<div
-					v-if="noticeData.contentType === 'list'"
-					class="info is-flex is-flex-direction-column is-justify-content-space-around is-flex-grow-1">
+				v-if="noticeData.contentType === 'list'"
+				class="info is-flex is-flex-direction-column is-justify-content-space-around is-flex-grow-1">
 				<div class="_widget-body is-flex mr-0">
 					<div class="image is-24x24 is-flex-shrink-0">
 						<img v-if="!noticeData.content[Object.keys(noticeData.content)[0]].icon"
 						     :src="require(`@/assets/img/logo/casa-white.svg`)" alt=""/>
-						<img v-else :src="require(`@/assets/img${noticeData.content[Object.keys(noticeData.content)[0]].icon}`)"
+						<img v-else
+						     :src="require(`@/assets/img${noticeData.content[Object.keys(noticeData.content)[0]].icon}`)"
 						     alt=""/>
 					</div>
 					<div class="body-title is-flex-grow-1 _nowrap ml-2">
@@ -53,7 +54,8 @@
 					<div class="image is-24x24 is-flex-shrink-0">
 						<img v-if="!noticeData.content[Object.keys(noticeData.content)[1]].icon"
 						     :src="require(`@/assets/img/logo/casa-white.svg`)" alt=""/>
-						<img v-else :src="require(`@/assets/img${noticeData.content[Object.keys(noticeData.content)[1]].icon}`)"
+						<img v-else
+						     :src="require(`@/assets/img${noticeData.content[Object.keys(noticeData.content)[1]].icon}`)"
 						     alt=""/>
 					</div>
 					<div class="body-title is-flex-grow-1 _nowrap ml-2">
@@ -72,15 +74,18 @@
 				<b-progress :value="noticeData.content.value" size="c-is-small" type="is-primary"></b-progress>
 			</div>
 			<!-- end of section body-->
-
+			
 			<!-- start of section footer-->
-			<div :style="{height: '24px'}" class="is-flex is-flex-direction-row-reverse is-flex-shrink-0 is-align-items-end">
+			<div :style="{height: '24px'}"
+			     class="is-flex is-flex-direction-row-reverse is-flex-shrink-0 is-align-items-end">
 				<template v-if="noticeData.contentType !== 'progress'">
-					<b-button v-if="!noticeData.operate" :disabled="false" class="width" rounded size="is-small" type="is-primary"
+					<b-button v-if="!noticeData.operate" :disabled="false" class="width" rounded size="is-small"
+					          type="is-primary"
 					          @click="close">
 						{{ $t('Cancel') }}
 					</b-button>
-					<b-button v-else-if="noticeData.operate.type === 'casaUI:eventBus'" :disabled="false" class="width" rounded
+					<b-button v-else-if="noticeData.operate.type === 'casaUI:eventBus'" :disabled="false" class="width"
+					          rounded
 					          size="is-small"
 					          type="is-primary"
 					          @click="eventBus">
@@ -152,16 +157,16 @@ export default {
 	},
 	inject: ['homeShowFiles'],
 	created() {
-
+	
 	},
 	beforeDestroy() {
 	},
 	computed: {},
-
+	
 	methods: {
 		close() {
 			this.$messageBus('youshouldknow_cardclose');
-
+			
 			if (this.noticeData.contentType === 'progress') {
 				this.$emit('deleteNotice', this.noticeData, this.noticeType);
 				return
@@ -184,8 +189,8 @@ export default {
 
 <style lang="scss" scoped>
 ._card {
-	height: 11.5rem;
-
+	height: 10rem;
+	
 	.widget-header {
 		.header-title {
 			font-family: 'Roboto';
@@ -196,11 +201,11 @@ export default {
 			color: hsla(208, 16%, 96%, 1);
 		}
 	}
-
+	
 	._widget-body {
 		align-items: center;
 		position: relative;
-
+		
 		.body-title {
 			font-family: 'Roboto';
 			font-style: normal;
@@ -210,7 +215,7 @@ export default {
 			color: hsla(208, 16%, 96%, 1);
 		}
 	}
-
+	
 	.footer-hint {
 		height: 16px;
 		font-family: 'Roboto';
@@ -223,17 +228,18 @@ export default {
 
 .content {
 	z-index: 10;
-	padding: 1rem;
+	padding-top: 1.125rem;
+	padding-bottom: 1rem;
 	padding-left: 1.25rem;
 	padding-right: 1.25rem;
-
+	
 	.info {
 		margin: 1.5rem 2rem;
-
+		
 		._ml-2rem {
 			margin-left: 2rem;
 		}
-
+		
 		.line {
 			width: auto;
 			border-top: 1px solid rgba(255, 255, 255, 0.1);
--- src/components/syncthing/SyncBlock.vue
+++ src/components/syncthing/SyncBlock.vue
@@ -7,163 +7,169 @@
   * Copyright (c) 2022 by IceWhale, All Rights Reserved.
   -->
 <template>
-  <div class="common-card">
-
-    <div class="blur-background"></div>
-    <div class="wuji-content _box is-flex is-flex-direction-column">
-      <!-- Init State Start -->
-      <h6 class="title is-4 mb-0 has-text-white is-flex-shrink-0">{{ $t(`Sync your data`) }}</h6>
-      <div class="is-flex is-align-items-center is-flex-grow-1">
-        <div class="info ">
-          <div class="des two-line is-size-14px">
-            {{ $t(`Use Syncthing to sync your files between multiple devices.`) }}
-          </div>
-        </div>
-        <b-image :src="require('@/assets/img/syncthing/sync.svg')" class="is-80x80"></b-image>
-      </div>
-      <div class="buttons is-flex is-flex-shrink-0 is-flex-direction-row-reverse">
-        <b-button class="mb-0" rounded size="is-small" type="is-primary" @click="openSyncPanel">{{
-            $t(actionText)
-          }}
-        </b-button>
-      </div>
-      <!-- Init State End -->
-
-    </div>
-  </div>
+	<div class="common-card">
+		
+		<div class="blur-background"></div>
+		<div class="wuji-content _box is-flex is-flex-direction-column">
+			<!-- Init State Start -->
+			<h6 class="title is-4 mb-0 has-text-white is-flex-shrink-0">{{ $t(`Sync your data`) }}</h6>
+			<div class="is-flex is-align-items-center is-flex-grow-1 _notice-content-text">
+				<div class="info ">
+					<div class="des two-line is-size-14px">
+						{{ $t(`Use Syncthing to sync your files between multiple devices.`) }}
+					</div>
+				</div>
+				<b-image :src="require('@/assets/img/syncthing/sync.svg')" class="is-80x80"></b-image>
+			</div>
+			<div class="buttons is-flex is-flex-shrink-0 is-flex-direction-row-reverse">
+				<b-button class="mb-0" rounded size="is-small" type="is-primary" @click="openSyncPanel">{{
+						$t(actionText)
+					}}
+				</b-button>
+			</div>
+			<!-- Init State End -->
+		
+		</div>
+	</div>
 </template>
 
 <script>
 import events from '@/events/events';
 
 export default {
-  name: "sync-block",
-  data() {
-    return {
-      isLoading: false,
-      isStarting: false,
-      syncBaseURL: "",
-      isSyncInstalled: false,
-      isSyncRunning: false,
-      syncPort: "",
-      syncId: ""
-    }
-  },
-  created() {
-    this.checkSyncStatus()
-
-    this.$EventBus.$on(events.UPDATE_SYNC_STATUS, () => {
-      this.checkSyncStatus();
-    });
-
-  },
-  beforeDestroy() {
-    this.$EventBus.$off(events.UPDATE_SYNC_STATUS);
-  },
-  computed: {
-    actionText() {
-      return !this.isSyncInstalled ? "Install" : "Open"
-    }
-  },
-
-  methods: {
-    async checkSyncStatus() {
-      // const res = await this.$api.sys.getSystemApps()
-      const listRes = await this.$api.container.getMyAppList();
-      const systemApps = listRes.data ? listRes.data.data.casaos_apps : [];
-      const is8384SyncInstalled = systemApps.some(app => {
-        return app.image.includes('syncthing') && app.port === 8384
-      })
-      if (is8384SyncInstalled) {
-        this.isSyncInstalled = true
-        this.syncBaseURL = `http://${this.$baseIp}:8384`
-        this.syncPort = 8384
-        this.syncId = systemApps.find(app => {
-          return app.image.includes('syncthing') && app.port === 8384
-        }).port
-        this.isSyncRunning = systemApps.some(app => {
-          return app.image.includes('syncthing') && app.port === 8384 && app.state === 'running'
-        })
-      } else {
-        this.isSyncInstalled = systemApps.some(app => {
-          return app.image.includes('syncthing')
-        })
-        if (this.isSyncInstalled) {
-          this.isSyncRunning = systemApps.some(app => {
-            return app.image.includes('syncthing') && app.state === "running"
-          })
-          this.syncPort = systemApps.find(app => {
-            return app.image.includes('syncthing')
-          }).port
-          this.syncId = systemApps.find(app => {
-            return app.image.includes('syncthing')
-          }).id
-          this.syncBaseURL = `http://${this.$baseIp}:${this.syncPort}`
-        }
-      }
-
-
-    },
-    async openSyncPanel() {
-      await this.checkSyncStatus()
-      if (!this.isSyncInstalled) {
-        this.$EventBus.$emit(events.OPEN_APP_STORE_AND_GOTO_SYNCTHING);
-      } else {
-        if (this.isSyncRunning) {
-          const arg = `\u003cscript\u003elocation.replace("${this.syncBaseURL}")\u003c/script\u003e`;
-          window.open('javascript:window.name;', arg);
-        } else {
-          this.$buefy.dialog.confirm({
-            title: ' ',
-            message: this.$t('Syncthing is not running, start it?'),
-            hasIcon: true,
-            closeOnConfirm: false,
-            confirmText: this.$t('Start'),
-            cancelText: this.$t('Cancel'),
-            onConfirm: (value, {close}) => {
-              this.$buefy.toast.open({
-                message: this.$t(`Starting Syncthing...`),
-                type: 'is-white'
-              })
-              this.$api.container.updateState(this.syncId, "start").then((res) => {
-                this.isStarting = false
-                if (res.data.success == 200) {
-                  this.$EventBus.$emit(events.RELOAD_APP_LIST);
-                  const arg = `\u003cscript\u003elocation.replace("${this.syncBaseURL}")\u003c/script\u003e`;
-                  setTimeout(() => {
-                    close()
-                    window.open('javascript:window.name;', arg);
-
-                  }, 2000)
-                } else {
-                  this.$buefy.toast.open({
-                    message: this.$t(`Failed to start, please try again.`),
-                    type: 'is-danger'
-                  })
-                }
-              })
-            }
-          })
-        }
-      }
-    },
-
-  },
-  sockets: {
-    "app:install-end"() {
-      this.checkSyncStatus();
-    },
-    "app:install-error"() {
-      this.checkSyncStatus();
-    },
-  }
+	name: "sync-block",
+	data() {
+		return {
+			isLoading: false,
+			isStarting: false,
+			syncBaseURL: "",
+			isSyncInstalled: false,
+			isSyncRunning: false,
+			syncPort: "",
+			syncId: ""
+		}
+	},
+	created() {
+		this.checkSyncStatus()
+		
+		this.$EventBus.$on(events.UPDATE_SYNC_STATUS, () => {
+			this.checkSyncStatus();
+		});
+		
+	},
+	beforeDestroy() {
+		this.$EventBus.$off(events.UPDATE_SYNC_STATUS);
+	},
+	computed: {
+		actionText() {
+			return !this.isSyncInstalled ? "Install" : "Open"
+		}
+	},
+	
+	methods: {
+		async checkSyncStatus() {
+			// const res = await this.$api.sys.getSystemApps()
+			const listRes = await this.$api.container.getMyAppList();
+			const systemApps = listRes.data ? listRes.data.data.casaos_apps : [];
+			const is8384SyncInstalled = systemApps.some(app => {
+				return app.image.includes('syncthing') && app.port === 8384
+			})
+			if (is8384SyncInstalled) {
+				this.isSyncInstalled = true
+				this.syncBaseURL = `http://${this.$baseIp}:8384`
+				this.syncPort = 8384
+				this.syncId = systemApps.find(app => {
+					return app.image.includes('syncthing') && app.port === 8384
+				}).port
+				this.isSyncRunning = systemApps.some(app => {
+					return app.image.includes('syncthing') && app.port === 8384 && app.state === 'running'
+				})
+			} else {
+				this.isSyncInstalled = systemApps.some(app => {
+					return app.image.includes('syncthing')
+				})
+				if (this.isSyncInstalled) {
+					this.isSyncRunning = systemApps.some(app => {
+						return app.image.includes('syncthing') && app.state === "running"
+					})
+					this.syncPort = systemApps.find(app => {
+						return app.image.includes('syncthing')
+					}).port
+					this.syncId = systemApps.find(app => {
+						return app.image.includes('syncthing')
+					}).id
+					this.syncBaseURL = `http://${this.$baseIp}:${this.syncPort}`
+				}
+			}
+			
+			
+		},
+		async openSyncPanel() {
+			await this.checkSyncStatus()
+			if (!this.isSyncInstalled) {
+				this.$EventBus.$emit(events.OPEN_APP_STORE_AND_GOTO_SYNCTHING);
+			} else {
+				if (this.isSyncRunning) {
+					const arg = `\u003cscript\u003elocation.replace("${this.syncBaseURL}")\u003c/script\u003e`;
+					window.open('javascript:window.name;', arg);
+				} else {
+					this.$buefy.dialog.confirm({
+						title: ' ',
+						message: this.$t('Syncthing is not running, start it?'),
+						hasIcon: true,
+						closeOnConfirm: false,
+						confirmText: this.$t('Start'),
+						cancelText: this.$t('Cancel'),
+						onConfirm: (value, {close}) => {
+							this.$buefy.toast.open({
+								message: this.$t(`Starting Syncthing...`),
+								type: 'is-white'
+							})
+							this.$api.container.updateState(this.syncId, "start").then((res) => {
+								this.isStarting = false
+								if (res.data.success == 200) {
+									this.$EventBus.$emit(events.RELOAD_APP_LIST);
+									const arg = `\u003cscript\u003elocation.replace("${this.syncBaseURL}")\u003c/script\u003e`;
+									setTimeout(() => {
+										close()
+										window.open('javascript:window.name;', arg);
+										
+									}, 2000)
+								} else {
+									this.$buefy.toast.open({
+										message: this.$t(`Failed to start, please try again.`),
+										type: 'is-danger'
+									})
+								}
+							})
+						}
+					})
+				}
+			}
+		},
+		
+	},
+	sockets: {
+		"app:install-end"() {
+			this.checkSyncStatus();
+		},
+		"app:install-error"() {
+			this.checkSyncStatus();
+		},
+	}
 }
 </script>
 
 <style lang="scss" scoped>
 ._box {
-  height: 11.5rem;
-  padding: 1rem 1.25rem;
-  margin-bottom: 1rem;
+	height: 10rem;
+	padding: 1rem 1.25rem;
+	padding-top: 1.125rem;
+	margin-bottom: 1rem;
+	
+	._notice-content-text {
+		margin-right: 2rem;
+		margin-left: 2rem;
+	}
 }
 </style>
