ab7bf2b722d42c17f185f53ae3380caf3d7ae326
zhanghengxin <ezreal.zhang@icewhale.org>
2023-04-14 18:25:04 +0800

    :alembic: Support i18n.
    Signed-off-by: zhanghengxin <ezreal.zhang@icewhale.org>

--- main/src/mixins/base/common-i18n.js
+++ main/src/mixins/base/common-i18n.js
@@ -12,6 +12,12 @@ let lang = localStorage.getItem('lang')
 const i18n = function (data, fallback = 'en_us') {
 	return data[lang] || data[fallback] || data['en_US']
 }
+
+// Common usage
+const ice_i18n = i18n
+export {ice_i18n};
+
+// vue usage
 export default {
 	methods: {
 		i18n
