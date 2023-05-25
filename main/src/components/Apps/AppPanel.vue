1bae6d770578894da16b179569a0e2bb6a55c7cc
Paulo Reis <paulovitin@gmail.com>
2023-05-25 00:15:30 -0300

    Hotfix/mobile modal height (#141)
    Fix the issue https://github.com/IceWhaleTech/CasaOS/issues/1084
    
    Note: I'm not a Vue developer, so I don't know if its way of doing
    things in VUE
    
    Prints here: https://github.com/IceWhaleTech/CasaOS-UI/pull/130

--- main/src/components/Apps/AppPanel.vue
+++ main/src/components/Apps/AppPanel.vue
@@ -692,6 +692,9 @@ export default {
 	},
 
 	created() {
+		window.addEventListener('resize', this.setCSSVHVar);
+		this.setCSSVHVar();
+
 		//Get recommend
 		this.getStoreRecommend();
 
@@ -827,6 +830,11 @@ export default {
 		}
 	},
 	methods: {
+		setCSSVHVar() {
+			const vh = window.innerHeight * 0.01;
+			document.documentElement.style.setProperty('--vh', `${vh}px`);
+		},
+  
 		// this.cateMenu : {name: 'appstore', title: 'App Store', icon: 'mdi-apps', component: 'AppStore'}
 		// param : this.cateMenu.name
 		getCateIcon(name) {
@@ -1450,6 +1458,7 @@ export default {
 	},
 
 	destroyed() {
+		window.addEventListener('resize', this.setCSSVHVar);
 		clearInterval(this.timer)
 	},
 
@@ -1650,7 +1659,7 @@ export default {
 
 @media screen and (max-width: 768px) {
 	.modal-card {
-		max-height: 100vh !important;
+		max-height: calc(var(--vh, 1vh) * 100);
 		border-radius: 0;
 	}
 }
