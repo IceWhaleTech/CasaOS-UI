03ddc91a9475131b37f45e81c234f896059ba7b8
WisdomSky <i.wisdomsky@gmail.com>
2023-06-16 17:02:38 +0800

    Add zoom screenshot image on click functionality (#166)
    **Reason for Implementation:**
    
    In current behavior, when a screenshot image is clicked, nothing
    happens. UX-wise, the user expects the image to enlarge when the images
    are clicked.
    
    **Screenshots:**
    
    
    ![image](https://github.com/IceWhaleTech/CasaOS-UI/assets/6792172/a6d47fd9-24f9-4e34-aac0-353d55d649e9)
    
    ![image](https://github.com/IceWhaleTech/CasaOS-UI/assets/6792172/370cecc5-70ee-4cd1-aac9-ec36c563924f)
    
    ![image](https://github.com/IceWhaleTech/CasaOS-UI/assets/6792172/8d56508a-0dfd-45ff-81fc-a44edcc83136)
    
    ---
    
    
    ![image](https://github.com/IceWhaleTech/CasaOS-UI/assets/6792172/e1b60463-c272-4ff1-bb04-80dda3efba66)
    
    ![image](https://github.com/IceWhaleTech/CasaOS-UI/assets/6792172/f6e2987b-5fbd-49ef-b91b-30ee9315677a)
    
    ![image](https://github.com/IceWhaleTech/CasaOS-UI/assets/6792172/8f887cd7-a361-4d9a-9d01-993da9a0ced5)
    
    
    ---
    
    ![image](https://github.com/IceWhaleTech/CasaOS-UI/assets/6792172/e5659744-7298-43e3-b28b-66dbadc96564)
    
    ![image](https://github.com/IceWhaleTech/CasaOS-UI/assets/6792172/f315aca7-b320-443b-b775-483607c9321f)
    
    ![image](https://github.com/IceWhaleTech/CasaOS-UI/assets/6792172/e94ca327-262f-4d6f-9bc4-bc737cf54384)
    
    ---------
    
    Signed-off-by: zhanghengxin <ezreal.zhang@icewhale.org>
    Co-authored-by: zhanghengxin <ezreal.zhang@icewhale.org>

--- main/src/components/Apps/AppDetailInfo.vue
+++ main/src/components/Apps/AppDetailInfo.vue
@@ -104,7 +104,8 @@
 						<div class="gap">
 							<b-image :src="item"
 									 :src-fallback="require('@/assets/img/app/swiper_placeholder.png')"
-									 class="border-8" placeholder ratio="16by9"></b-image>
+									 class="border-8"
+									 placeholder ratio="16by9" @click.native="zoomScreenshot(item)"></b-image>
 						</div>
 					</swiper-slide>
 
@@ -237,11 +238,57 @@ export default {
 			let tempO = this.cateMenu.find(item => item.name == name) || {font: 'apps'}
 			return tempO.font;
 		},
+		zoomScreenshot(img) {
+			const customVNode = this.$createElement('div', {
+				class: 'modal-content'
+			}, [
+				this.$createElement('img', {attrs: {src: img}})
+			]);
 
+
+			this.$buefy.modal.open({
+				content: [customVNode],
+				customClass: '_zoom-screenshot',
+				fullScreen: true,
+				hasModalCard: true,
+				destroyOnHide: true,
+				animation: "zoom-in",
+				canCancel: ["escape", "outside", "x"]
+			});
+		}
 	}
 }
 </script>
 
-<style scoped>
+<style lang="scss">
+//The underscore "_" here represents that it is only used in this context and needs to be placed in the modularized CSS later.
+.modal._zoom-screenshot {
 
+	.animation-content {
+		display: flex;
+		align-items: center;
+	}
+
+	.modal-content {
+		width: auto;
+		overflow: unset;
+
+		img {
+			max-width: 90vw;
+			max-height: 90vh;
+			border: 3px solid #ccc;
+			border-radius: 1rem;
+			box-shadow: 0 0 40px 0 rgba(255, 255, 255, 0.5);
+		}
+	}
+
+	.modal-close {
+		position: absolute;
+
+		&::before, &::after {
+			background: #fff;
+		}
+	}
+
+}
 </style>
