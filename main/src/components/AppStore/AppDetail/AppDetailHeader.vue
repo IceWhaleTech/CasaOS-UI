e9e1dc10d618d46960a3ecb5d815cb43a4309daa
CorrectRoadH <a778917369@gmail.com>
2023-07-22 00:15:11 +0800

    eslint

--- main/src/components/AppStore/AppDetail/AppDetailHeader.vue
+++ main/src/components/AppStore/AppDetail/AppDetailHeader.vue
@@ -1,5 +1,5 @@
 <template>
-	<header class="modal-card-head" style="background:#ff000">
+	<header class="modal-card-head">
 		<div class="is-flex-grow-1">
 			<div class="button is-ghost auto-height pl-0 pt-0 pb-0" @click="close">
 				<b-icon class="mr-1" icon="chevron-left" size="is-medium"></b-icon>
@@ -8,6 +8,7 @@
 		</div>
 	</header>
 </template>
+
 <script setup>
 import { defineProps } from 'vue';
 const props = defineProps({
@@ -18,4 +19,3 @@ const props = defineProps({
 	},
 });
 </script>
-
