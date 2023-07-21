e1232c86fa8a80581874bac032d86995d794b823
CorrectRoadH <a778917369@gmail.com>
2023-07-22 00:34:05 +0800

    eslint

--- main/src/components/AppStore/index.js
+++ main/src/components/AppStore/index.js
@@ -1,10 +1,2 @@
-/**
- * @file index.js
- * @description 
- * @author 
- * @copyright 
- */
-
-import AppStoreBanner from "./AppStoreBanner.vue";
 import AppDetail from "./AppDetail";
-export { AppStoreBanner, AppDetail }
+export { AppDetail }
