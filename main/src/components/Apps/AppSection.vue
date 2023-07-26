0eae5d07484a3765202a7c0dd14c4471b46beb74
zhanghengxin <ezreal.zhang@icewhale.org>
2023-07-26 19:05:06 +0800

    :pencil2: Fixing typos.
    Signed-off-by: zhanghengxin <ezreal.zhang@icewhale.org>

--- main/src/components/Apps/AppSection.vue
+++ main/src/components/Apps/AppSection.vue
@@ -61,7 +61,7 @@
 			<!-- Title Bar Start -->
 			<div class="title-bar is-flex is-align-items-center mt-2rem  mb-5">
 				<app-section-title-tip id="appTitle2" class="is-flex-grow-1 has-text-sub-04" label="Need to be rebuild."
-									   title="Legacy app">
+									   title="Legacy app(Need to be rebuild).">
 				</app-section-title-tip>
 			</div>
 			<!-- Title Bar End -->
