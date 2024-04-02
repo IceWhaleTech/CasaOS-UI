276e94b6a5b05898ba870e6d64c7a79a212e3806
zhanghengxin <ezreal.zhang@icewhale.org>
2024-04-02 17:12:26 +0800

    Remove console.error statement in ComposeConfig.vue

--- main/src/components/Apps/ComposeConfig.vue
+++ main/src/components/Apps/ComposeConfig.vue
@@ -427,7 +427,6 @@ export default {
 						this.serviceStableVersion = res.data.data.tag;
 					})
 					.catch((e) => {
-						console.error(e);
 						this.serviceStableVersion = "";
 					});
 				}else{
